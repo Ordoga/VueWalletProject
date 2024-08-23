4ddec20e4ff67ce90e8719faa6fefb3f80ca8369
Ordoga <ordogga@gmail.com>
2024-08-23 23:48:44 +0300

    Back Button, Details design

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -55,6 +55,67 @@ ul {
     margin:0px;
 }
 
+
+.contact-card-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    grid-auto-rows: 1fr;
+    gap: 10px;
+    grid-auto-flow: dense;
+    justify-content: center;
+    
+}
+
+.contact-card {
+    position: relative;
+    aspect-ratio: 2/2.5;
+    
+    background: linear-gradient(142deg,rgb(118, 232, 255) 0%, rgba(0,212,255,1) 70%);
+    
+    padding: 1rem;
+    border-radius: 16px;
+    
+    &:hover{
+        background: linear-gradient(142deg,rgb(118, 232, 255) 0%, rgb(137, 235, 255) 70%);
+
+        // box-shadow: 8px 8px 8px 8px rgba(255, 255, 255, 0.2), 8px 8px rgba(14, 211, 255, 0.35);
+    }
+}
+
+.card-content{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap:16px;
+    height: 100%;
+    justify-content: space-around;
+    /* position: absolute; */
+    /* top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0; */
+}
+
+
+
+.list-move, /* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.7s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+//   transform: translateX(30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+animations can be calculated correctly. */
+.list-leave-active {
+  position: absolute;
+}
+
 .btn {
   appearance: button;
   background-color: #1899D6;
@@ -130,59 +191,4 @@ ul {
         }
     }
 }
-
-.contact-card-grid {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-    grid-auto-rows: 1fr;
-    gap: 10px;
-    grid-auto-flow: dense;
-    justify-content: center;
-
-}
-
-.contact-card {
-    position: relative;
-    aspect-ratio: 2/2.5;
-
-    background: linear-gradient(142deg,rgb(118, 232, 255) 0%, rgba(0,212,255,1) 70%);
-
-    padding: 1rem;
-    border-radius: 16px;
-}
-
-.card-content{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap:16px;
-    height: 100%;
-    justify-content: space-around;
-    /* position: absolute; */
-    /* top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0; */
-}
-
-
-
-.list-move, /* apply transition to moving elements */
-.list-enter-active,
-.list-leave-active {
-  transition: all 0.7s ease;
-}
-
-.list-enter-from,
-.list-leave-to {
-  opacity: 0;
-//   transform: translateX(30px);
-}
-
-/* ensure leaving items are taken out of layout flow so that moving
-   animations can be calculated correctly. */
-.list-leave-active {
-  position: absolute;
-}
-
 </style>
--- src/pages/ContactDetails.vue
+++ src/pages/ContactDetails.vue
@@ -1,8 +1,19 @@
 <template>
-    <section v-if="contact" class="contact-details page">
-        <div>{{ contact.name }}</div>
-        <div>{{ contact.email }}</div>
+    <section v-if="contact" class="contact-details-page page">
+        <div class="contact-details">
+            <div class="contact-name">{{ contact.name }}</div>
+            <img :src="`https://robohash.org/${ contact.name }?set=set4`">
+            <div class="email-phone">
+                <div class="contact-email">{{ contact.email }}</div>
+                <div class="contact-phone">{{ contact.phone }}</div>
+            </div>
+        </div>
+        
+        <div class="actions">
+            <RouterLink to="/contact">Back</RouterLink>
+        </div>
     </section>
+
     <section v-else>Loading...</section>
 </template>
 
@@ -27,6 +38,43 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.contact-details-page{
+    display: flex;
+    flex-direction: column;
+    align-items:center;
+}
+
+.contact-details {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    width: 500px;
+    height: 500px;
+    border-radius: 24px;
+    background-color: rgba(0,212,255,1);
+    margin-bottom: 8px;
+
+    & .contact-name {
+        font-size: 1.4rem;
+    }
+
+    & .email-phone {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 8px;
+    }
+}
+
+a {
+    color: rgb(0, 212, 255);
+
+    &:hover{
+        color: white;
+    }
+}
 
 </style>
