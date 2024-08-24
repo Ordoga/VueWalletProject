0440a7291f275d8483dad32b6fb95bc4842dd0ed
Ordoga <ordogga@gmail.com>
2024-08-24 16:16:40 +0300

    Statistics page built - Not finished

---
+++ src/components/LineChart.vue
@@ -0,0 +1,30 @@
+<template>
+    <Line
+
+    />
+
+</template>
+
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+    },
+    created() {
+
+    },
+    components: {
+        Line
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
--- src/pages/Statistics.vue
+++ src/pages/Statistics.vue
@@ -1,21 +1,45 @@
 <template>
-    <div class="statistics-page page">
-        Statistics
+    <div v-if="marketPriceHistory" class="statistics-page page">
+        {{  formattedData }}
+        <LineChart
+            :data="formattedData"
+        />
     </div>
 </template>
 
 <script>
+import LineChart from '../components/LineChart.vue'
+import { bitcoinService } from '../services/bitcoinService.js'
+
 export default {
     data() {
         return {
-            
+            marketPriceHistory: null
         }
     },
     methods: {
     },
-    created() {
+    async created() {
+        this.marketPriceHistory = await bitcoinService.getMarketPriceHistory()
     },
     components: {
+        LineChart
+    },
+    computed: {
+        formattedData(){
+            let values = this.marketPriceHistory.values;
+            const dates = []
+            const prices = []
+            values = values.map((xyObj) => {
+                const date = new Date(xyObj.x * 1000)   
+                const day = String(date.getDay()).padStart(2, '0');
+                const month = String(date.getMonth() + 1).padStart(2, '0');
+                dates.push(`${day}/${month}`)
+                prices.push(xyObj.y)
+            })
+            const dataset = { data : prices}
+            return { datasets: [dataset], labels : dates}
+        }
     }
 }
 </script>
